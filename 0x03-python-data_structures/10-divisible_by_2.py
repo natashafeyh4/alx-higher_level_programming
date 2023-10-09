931983d5b4d2157d8d11e67ff19fbbcd3933dbfa
natashafeyh4 <natashafeyh44@gmail.com>
2023-10-09 11:34:05 -0700

    10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,12 +1,14 @@
-def max_integer(my_list=[]):
-    if not my_list:
-        return None
-    max_val = my_list[0]
-    for item in my_list:
-        if item > max_val:
-            max_val = item
-    return max_val
+#!/usr/bin/python3
+# 10-divisible_by_2.py
 
-my_list = [1, 90, 2, 13, 34, 5, -13, 3]
-max_value = max_integer(my_list)
-print("Max: {}".format(max_value))
+
+def divisible_by_2(my_list=[]):
+    """Find all multiples of 2 in a list."""
+    multiples = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            multiples.append(True)
+        else:
+            multiples.append(False)
+
+    return (multiples)
