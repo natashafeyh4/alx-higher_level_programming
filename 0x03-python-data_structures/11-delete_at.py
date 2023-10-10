c59baa41615b85d11a815398b57172ab7e75f52f
natashafeyh4 <natashafeyh44@gmail.com>
2023-10-09 19:55:17 -0700

    11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 # 11-delete_at.py
 
-def divisible_by_2(my_list=[]):
-    return [item % 2 == 0 for item in my_list]
 
-my_list = [0, 1, 2, 3, 4, 5, 6]
-list_result = divisible_by_2(my_list)
-
-i = 0
-while i < len(list_result):
-    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
-    i += 1
+def delete_at(my_list=[], idx=0):
+    """Delete an item at a specific position in a list."""
+    if idx >= 0 and idx < len(my_list):
+        del my_list[idx]
+    return (my_list)
