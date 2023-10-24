e23000d2718e2274f7eee446578c27e46571cded
natashafeyh4 <natashafeyh44@gmail.com>
2023-10-24 14:48:02 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+class Square:
+    """Class to define a square."""
+    
+    def __init__(self, size):
+        """Initialize the square."""
+        self.__size = size
