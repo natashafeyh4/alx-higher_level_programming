21ef1a14d1c5dc8968bc1821474c52aa95412ede
natashafeyh4 <natashafeyh44@gmail.com>
2023-11-01 03:20:50 -0700

    update

--- alx-higher_level_programming/0x08-python-more_classes/4-rectangle.py
+++ alx-higher_level_programming/0x08-python-more_classes/4-rectangle.py
@@ -1,16 +1,34 @@
 #!/usr/bin/python3
+"""Defines a Rectangle class"""
+
 
 class Rectangle:
+    """Rectangle class"""
+
     def __init__(self, width=0, height=0):
+        """Initializes the instance attributes to create a rectangle
+            Args:
+                width: size of the square
+                height: height of rectangle
+        """
+
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Gets the width of the rectangle
+           Returns:
+                int: width of the rectangle class instance
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Sets the width of the rectangle
+        Args:
+            int: value to set as width
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -19,10 +37,20 @@ class Rectangle:
 
     @property
     def height(self):
+        """Gets the height of the rectangle
+           Returns:
+                int: height of the rectangle class instance
+        """
+
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Sets the width of the rectangle
+        Args:
+            int: value to set as width
+        """
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -30,18 +58,78 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        return self.__width * self.__height
+        """ Calculates area of rectangle area (h * w)
+            Returns:
+                int: Rectangle area calculated
+        """
+        return self.__height * self.__width
 
     def perimeter(self):
-        return (self.__width + self.__height) * 2
+        """ Calculates perimeter of rectangle perimeter 2 * (h + w)
+            Returns:
+                int: Rectangle perimeter calculated
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
+
+    def area(self):
+        """ Calculates area of rectangle area (h * w)
+            Returns:
+                int: Rectangle area calculated
+        """
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """ Calculates perimeter of rectangle perimeter 2 * (h + w)
+            Returns:
+                int: Rectangle perimeter calculated
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        return ("#" * self.__width + "\n") * self.__height
+        """Called to return a string represention of
+           the rectangle when printed
+           Returns:
+                str: Representation of the rectangel using (#) symbol
+        """
+        return (("#" * self.__width + "\n") * self.__height)[:-1]
 
     def __repr__(self):
+        """Called to return a string represention of
+           the rectangle that could be called using eval
+           Returns:
+                str: Representation of the rectangle
+        """
+
         return "Rectangle({}, {})".format(self.__width, self.__height)
 
-    def __del__(self):
-        print("Bye rectangle...")
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle(2, 4)
+    print(str(my_rectangle))
+    print("--")
+    print(my_rectangle)
+    print("--")
+    print(repr(my_rectangle))
+    print("--")
+    print(hex(id(my_rectangle)))
+    print("--")
+
+    # create new instance based on representation
+    new_rectangle = eval(repr(my_rectangle))
+    print(str(new_rectangle))
+    print("--")
+    print(new_rectangle)
+    print("--")
+    print(repr(new_rectangle))
+    print("--")
+    print(hex(id(new_rectangle)))
+    print("--")
+
+    print(new_rectangle is my_rectangle)
+    print(type(new_rectangle) is type(my_rectangle))
