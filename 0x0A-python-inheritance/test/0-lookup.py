aeeab1fbaef0fbfc3111bfb185eb7c732d3aa982
natashafeyh4 <natashefeyh44@gmail.com>
2023-11-06 18:45:14 +0000

    update

---
+++ 0x0A-python-inheritance/test/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+0x0A. Python - Inheritance
+"""
+
+
+def lookup(obj):
+    """A function that returns the list of available
+    attributes and methods of an object."""
+    return dir(obj)
