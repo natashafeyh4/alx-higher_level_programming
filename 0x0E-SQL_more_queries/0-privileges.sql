1e2af3a87c5bbeedac1a0370c94a2dfe3f59419e
natashafeyh4 <natashafeyh44@gmail.com>
2023-12-16 00:12:51 -0800

    tasks all

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- list all privileges of users user_0d_1 and user_0d_2
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost)
+
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
