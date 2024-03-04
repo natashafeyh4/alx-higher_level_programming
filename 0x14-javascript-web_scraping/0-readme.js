2ec65aa161c98180c806485ff121ecc0d720dfc6
natashafeyh4 <natashafeyh44@gmail.com>
2024-03-04 06:46:52 +0000

    update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,18 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
-});
+
+process.argv[0] = 'node';
+function readFile (filePath) {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(data);
+  });
+}
+
+if (process.argv.length === 3) {
+  const filePath = process.argv[2];
+  readFile(filePath);
+}
--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,1 @@
-#!/usr/bin/node
-const request = require('request');
-request.get(process.argv[2]).on('response', function (response) {
-  console.log(`code: ${response.statusCode}`);
-});
+
