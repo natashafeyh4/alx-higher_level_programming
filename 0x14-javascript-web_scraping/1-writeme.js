0ffe928a2be8756ac9dd517c7ca198aeae7006cf
natashafeyh4 <natashafeyh44@gmail.com>
2024-03-04 06:49:42 +0000

    update

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
+
 const fs = require('fs');
+const filename = process.argv[2];
+const content = process.argv[3];
 
-function writeFile (filePath, content) {
-  fs.writeFile(filePath, content, 'utf8', (err) => {
-    if (err) {
-      console.error(err);
-    }
-  });
-}
-
-if (process.argv.length === 4) {
-  const filePath = process.argv[2];
-  const content = process.argv[3];
-  writeFile(filePath, content);
-}
+fs.writeFile(filename, content, 'utf-8', (error) => {
+  if (error) {
+    console.log(error);
+  }
+});
